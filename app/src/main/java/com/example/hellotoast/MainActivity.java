cd95ca0272083e65fa704f159d57d0b449c9be56
Alfonso Carrillo <alfonso.carrillo@AlphaCredit.local>
2019-10-21 09:24:44 -0500

    Homework button zero

--- app/src/main/java/com/example/hellotoast/MainActivity.java
+++ app/src/main/java/com/example/hellotoast/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.hellotoast;
 
+import androidx.annotation.ColorInt;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -10,12 +14,14 @@ public class MainActivity extends AppCompatActivity {
 
     private int mCount = 0;
     private TextView mShowCount;
+    private Button buttonToZero;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mShowCount = (TextView) findViewById(R.id.show_count);
+        buttonToZero = findViewById(R.id.button_zero);
     }
 
     public void showToast(View view) {
@@ -28,6 +34,17 @@ public class MainActivity extends AppCompatActivity {
         mCount++;
         if (mShowCount != null) {
             mShowCount.setText(Integer.toString(mCount));
+        }
+        buttonToZero.setBackgroundColor(Color.GRAY);
+        buttonToZero.setTextColor(Color.BLACK);
+    }
+
+    public void countToZero(View view) {
+        buttonToZero.setBackgroundColor(Color.BLACK);
+        buttonToZero.setTextColor(Color.WHITE);
+
+        if (mShowCount != null) {
+            mShowCount.setText("0");
         }
     }
 }
